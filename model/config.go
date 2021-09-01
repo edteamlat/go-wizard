55c14d406ee8c09df7afa7a324684e7270fa90d3
Hernan Reyes <hernan_rm@outlook.es>
2021-09-01 13:06:13 -0600

    Implements template tests for every layer (#2)
    * Adds domain template tests
    
    * Adds usecase template tests
    
    * Adds model template tests
    
    * Adds sqlmigration template tests
    
    * Implements filesystem
    
    * Adds the handler templates
    
    * Adds table comment on config file

--- model/config.go
+++ model/config.go
@@ -2,9 +2,11 @@ package model
 
 // Config model to map configuration from a yaml file
 type Config struct {
-	ProjectPath string   `yaml:"project_path"`
-	Model       string   `yaml:"module"`
-	Table       string   `yaml:"table"`
-	Layers      []string `yaml:"layers"`
-	Fields      Fields   `yaml:"fields"`
+	ProjectPath  string   `yaml:"project_path"`
+	ModuleName   string   `yaml:"module_name"`
+	Model        string   `yaml:"model"`
+	Table        string   `yaml:"table"`
+	TableComment string   `yaml:"table_comment"`
+	Layers       []string `yaml:"layers"`
+	Fields       Fields   `yaml:"fields"`
 }
